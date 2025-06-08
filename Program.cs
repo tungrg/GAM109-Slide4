2aa7db864c067a6d8caefc1df6416af9004cdd02
tungrg <tungrg07@gmail.com>
2025-06-08 12:39:21 +0700

    add slide 5

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using Slide5;
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 public class Program
 {
@@ -65,19 +66,59 @@ public class Program
         //    }
         //}
         var listStudent = new List<Student>();
-        listStudent.Add(new Student(1,"NguyenVanA",6.5f));
-        listStudent.Add(new Student(2, "NguyenVanB", 7.5f));
-        listStudent.Add(new Student(3, "NguyenVanC", 8.5f));
+        listStudent.Add(new Student(1,"NguyenVanA",7.5f));
+        listStudent.Add(new Student(99, "NguyenVanF", 0.5f));
+        listStudent.Add(new Student(2, "NguyenVanB", 6.5f));
+        listStudent.Add(new Student(3, "NguyenVanC", 0.5f));
         listStudent.Add(new Student(4, "NguyenVanD", 9.5f));
+        listStudent.Add(new Student(5, "NguyenVanE", 9.5f));
+        ////VIet 1 cau truy van tim tat ca sinh vien co diem > 8
+        //var ketQua = from student in listStudent // Lay nguon du lieu
+        //             where student.Grade > 8f // dieu kien
+        //             select student; // khi gap dieu kien thi lam gi
+        //foreach(Student student in ketQua)
+        //{
+        //    Console.WriteLine($"Id: {student.Id}, Name : {student.Name}, Grade: {student.Grade}");
+        //}
+        //var minGrade = listStudent.Sum(x => x.Grade);
+        ////foreach (var student in minGrade)
+        ////{
+        ////    Console.WriteLine($"Id: {student.Id}, Name : {student.Name}, Grade: {student.Grade}");
+        ////}
+        //Console.WriteLine(minGrade);
+        //int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
 
-        //VIet 1 cau truy van tim tat ca sinh vien co diem > 8
-        var ketQua = from student in listStudent
-                     where student.Grade > 8f
-                     select student;
-        foreach(Student student in ketQua)
+        //var result = a.TakeWhile(x => x < 5);
+        //result.ToList().ForEach(x => Console.WriteLine(x));
+        //var lookUpStudent = listStudent.ToLookup(x =>x.Id);
+        //foreach(var student in lookUpStudent)
+        //{
+        //    Console.WriteLine(student.Key);
+        //    foreach(var info in student)
+        //    {
+        //        Console.WriteLine($"Name: {info.Name}, Grade: {info.Grade}");
+        //    }
+        //}
+        //int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+        //try
+        //{
+        //    var b = a.Cast<float>();
+        //    foreach (var item in b)
+        //    {
+        //        Console.WriteLine(item);
+        //    }
+        //}
+        //catch (Exception ex)
+        //{
+        //    Console.WriteLine("Khong ep kieu duoc");
+        //}
+        int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+        var b = a.AsEnumerable();
+        foreach(var x in b)
         {
-            Console.WriteLine($"Id: {student.Id}, Name : {student.Name}, Grade: {student.Grade}");
+            Console.WriteLine(x);
         }
+
     }
     
     
